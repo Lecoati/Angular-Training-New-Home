f8a927e74bfd02e91d75f26227f50d5c00bd6c98
Cristhian Amaya <cristhian@lecoati.com>
2015-10-11 22:27:42 +0200

    Ejercicio 08
    * Añadir función en el controlador home para agregar un like a un animal
    * Usar ng-click en el icono del corazón para agregar un like

--- app/data/animals.service.js
+++ app/data/animals.service.js
@@ -5,43 +5,19 @@
         .module('newHome.data')
         .factory('animalsService', animalsService);
 
-    animalsService.$inject = ['$q'];
+    animalsService.$inject = ['$http', 'API_ENDPOINT'];
 
-    function animalsService($q) {
+    function animalsService($http, API_ENDPOINT) {
         var service = {
             getAll: getAll,
         };
         return service;
 
         function getAll() {
-            var deferred = $q.defer();
-
-            deferred.resolve([{
-                id: 1,
-                photo: 'http://lorempixel.com/1024/600/cats/1',
-                name: 'Baco',
-                description: 'lorem ipsum dolor sit amet',
-                likes: 0,
-                commentsCount: 0
-            },
-            {
-                id: 2,
-                photo: 'http://lorempixel.com/1024/600/cats/2',
-                name: 'Garfield',
-                description: 'lorem ipsum dolor sit amet',
-                likes: 4,
-                commentsCount: 10
-            },
-            {
-                id: 3,
-                photo: 'http://lorempixel.com/1024/600/cats/3',
-                name: 'Malibu',
-                description: 'lorem ipsum dolor sit amet',
-                likes: 9,
-                commentsCount: 1
-            }]);
-
-            return deferred.promise;
+            return $http.get(API_ENDPOINT)
+                .then(function(response) {
+                    return response.data;
+                });
         }
 
     }
