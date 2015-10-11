ba8376de850c16c0ce9410e74024005fecf7318c
Cristhian Amaya <cristhian@lecoati.com>
2015-10-11 22:21:49 +0200

    Ejercicio 06
    * Inyectar el servicio $q en nuestro animalsService
    * Modificar método getAll para que devuelva una promise
    * Modificar controlador home para obtener los datos de la promise

--- app/home/home.controller.js
+++ app/home/home.controller.js
@@ -5,33 +5,18 @@
         .module('newHome.home')
         .controller('HomeController', HomeController);
 
-    function HomeController() {
+    HomeController.$inject = ['animalsService'];
+
+    function HomeController(animalsService) {
         var vm = this;
 
-        vm.animals = [{
-            id: 1,
-            photo: 'http://lorempixel.com/1024/600/cats/1',
-            name: 'Baco',
-            description: 'lorem ipsum dolor sit amet',
-            likes: 0,
-            commentsCount: 0
-        },
-        {
-            id: 2,
-            photo: 'http://lorempixel.com/1024/600/cats/2',
-            name: 'Garfield',
-            description: 'lorem ipsum dolor sit amet',
-            likes: 4,
-            commentsCount: 10
-        },
-        {
-            id: 3,
-            photo: 'http://lorempixel.com/1024/600/cats/3',
-            name: 'Malibu',
-            description: 'lorem ipsum dolor sit amet',
-            likes: 9,
-            commentsCount: 1
-        }];
+        vm.animals = [];
+
+        activate();
+
+        function activate() {
+            vm.animals = animalsService.getAll();
+        }
     }
 
 })();
