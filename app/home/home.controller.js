d5c9c8a39492db72e28c000ca42c50f18219f200
Cristhian Amaya <cristhian@lecoati.com>
2015-10-11 22:15:13 +0200

    Ejercicio 04
    * Usar ng-repeat sobre la lista de animales
    * Mostrar los datos del animal en la vista

--- app/home/home.controller.js
+++ app/home/home.controller.js
@@ -8,7 +8,31 @@
     function HomeController() {
         var vm = this;
 
-        vm.title = 'New Home';
+        vm.animals = [{
+            id: 1,
+            photo: 'http://lorempixel.com/1024/600/cats/1',
+            name: 'Baco',
+            description: 'lorem ipsum dolor sit amet',
+            likes: 0,
+            commentsCount: 0
+        },
+        {
+            id: 2,
+            photo: 'http://lorempixel.com/1024/600/cats/2',
+            name: 'Garfield',
+            description: 'lorem ipsum dolor sit amet',
+            likes: 4,
+            commentsCount: 10
+        },
+        {
+            id: 3,
+            photo: 'http://lorempixel.com/1024/600/cats/3',
+            name: 'Malibu',
+            description: 'lorem ipsum dolor sit amet',
+            likes: 9,
+            commentsCount: 1
+        }];
     }
+
 })();
 
